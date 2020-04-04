204947393e8d93f5399d9bb7c0dacfa3dbbd222a
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-04-04 16:39:09 -0600

    u

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -110,11 +110,11 @@
   </section>
 
   <!-- Portfolio Grid -->
-  <section class="bg-light page-section" id="portfolio">
+   <!--<section class="bg-light page-section" id="portfolio">
     <div class="container">
       <div class="row">
         <div class="col-lg-12 text-center">
-          <h2 class="section-heading text-uppercase">Portfolio</h2>
+          <h2 class="section-heading text-uppercase">Portafolio</h2>
           <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
         </div>
       </div>
@@ -205,10 +205,10 @@
         </div>
       </div>
     </div>
-  </section>
+  </section>-->
 
   <!-- About -->
-  <section class="page-section" id="about">
+  <section class="bg-light page-section" id="about">
     <div class="container">
       <div class="row">
         <div class="col-lg-12 text-center">
