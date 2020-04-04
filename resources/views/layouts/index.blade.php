3630f2271c52cac54ffae1d4bb247e7073348d13
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-04-04 16:20:00 -0600

    update

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -83,6 +83,7 @@
             <i class="fas fa-circle fa-stack-2x text-primary"></i>
             <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
           </span>
+          
           <h4 class="service-heading">Emergencia</h4>
           <p class="text-muted"></p>
         </div>
@@ -91,7 +92,15 @@
             <i class="fas fa-circle fa-stack-2x text-primary"></i>
             <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
           </span>
-          <h4 class="service-heading">Responsive Design</h4>
+          <h4 class="service-heading">Preventivo</h4>
+          <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+        </div>
+        <div class="col-md-4">
+          <span class="fa-stack fa-4x">
+            <i class="fas fa-circle fa-stack-2x text-primary"></i>
+            <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 class="service-heading">Correctivo</h4>
           <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
         </div>
 
