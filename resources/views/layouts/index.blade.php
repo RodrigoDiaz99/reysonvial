3b9ab1569eae6008ee514afd16b8efe7ecd0841f
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-04-04 16:23:54 -0600

    u

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -81,9 +81,10 @@
         <div class="col-md-4">
           <span class="fa-stack fa-4x">
             <i class="fas fa-circle fa-stack-2x text-primary"></i>
-            <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+            <i class="fas fa-first-aid fa-stack-1x fa-inverse"></i>
+
           </span>
-          
+
           <h4 class="service-heading">Emergencia</h4>
           <p class="text-muted"></p>
         </div>
