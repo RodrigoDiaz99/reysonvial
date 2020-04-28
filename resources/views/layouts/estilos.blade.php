de9e9a176c8179a3c4080c6ef44ef9f03c593472
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-04-28 17:51:07 -0500

    update

--- resources/views/layouts/estilos.blade.php
+++ resources/views/layouts/estilos.blade.php
@@ -799,19 +799,19 @@
     <!-- ***** Footer Area Start ***** -->
 
     <!-- Jquery-2.2.4 JS -->
-    <script src="js/jquery-2.2.4.min.js"></script>
+    <script src="/js/jquery-2.2.4.min.js"></script>
     <!-- Popper js -->
-    <script src="js/popper.min.js"></script>
+    <script src="/js/popper.min.js"></script>
     <!-- Bootstrap-4 Beta JS -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
     <!-- All Plugins JS -->
-    <script src="js/plugins.js"></script>
+    <script src="/js/plugins.js"></script>
     <!-- Slick Slider Js-->
-    <script src="js/slick.min.js"></script>
+    <script src="/js/slick.min.js"></script>
     <!-- Footer Reveal JS -->
-    <script src="js/footer-reveal.min.js"></script>
+    <script src="/js/footer-reveal.min.js"></script>
     <!-- Active JS -->
-    <script src="js/active.js"></script>
+    <script src="/js/active.js"></script>
 </body>
 
 </html>
