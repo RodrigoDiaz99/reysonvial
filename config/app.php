46aaf0009514309bc13f184819ce140f3ed46ab6
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-04-30 23:14:33 -0500

    update

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'ReysonVial App'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+   // 'url' => env('APP_URL', 'http://localhost'),
+   'url' => env('APP_URL', 'https://www.reysonvial.xyz/'),
 
     'asset_url' => env('ASSET_URL', null),
 
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('inicio', 'InicioController'); //para utilizar todas las rutas
+Auth::routes();
