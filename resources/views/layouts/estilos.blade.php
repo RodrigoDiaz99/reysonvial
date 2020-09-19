2297870490f3fa027cd8141c045c5b4e83463a92
Rodrigo Enrique Diaz Serviran <47422694+RodrigoDiaz99@users.noreply.github.com>
2020-09-18 22:27:15 -0500

    comit

--- resources/views/layouts/estilos.blade.php
+++ resources/views/layouts/estilos.blade.php
@@ -586,6 +586,13 @@
         <div class="copyright-text">
             <!-- ***** Removing this text is now allowed! This template is licensed under CC BY 3.0 ***** -->
             <p>Copyright ©2020 Rv. Designed by <a href="https://www.reysonvial.xyz/" target="_blank">ReysonVial</a></p>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick" />
+<input type="hidden" name="hosted_button_id" value="KVJB9BW477Y7S" />
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+</form>
+
         </div>
     </footer>
     <!-- ***** Footer Area Start ***** -->
